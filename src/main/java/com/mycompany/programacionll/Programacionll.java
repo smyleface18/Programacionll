2e56688e03f83abbb71a4bf8c49f4326a4c5dbdd
Caleb Acosta <ca30850@gmail.com>
2024-02-29 14:33:19 -0500

    29 febrero

---
+++ src/main/java/com/mycompany/programacionll/Programacionll.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programacionll;
+
+/**
+ *
+ * @author SCIS2PC08
+ */
+public class Programacionll {
+
+    public static void main(String[] args) {
+        
+                String hola [] ={"hola","hello"};
+        
+        for(String saludo:hola){
+            System.out.println("saludo = " + saludo);
+        }
+    }
+}
