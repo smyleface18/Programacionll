666f151510effb96134088944b380dd9ef3639f0
Caleb Acosta <ca30850@gmail.com>
2024-02-29 15:19:15 -0500

    cración de clases interna

--- src/main/java/com/mycompany/programacionll/Programacionll.java
+++ src/main/java/com/mycompany/programacionll/Programacionll.java
@@ -10,12 +10,35 @@ package com.mycompany.programacionll;
  */
 public class Programacionll {
 
+//    Error: LinkageError occurred while loading main class com.mycompany.javau.feb_29 java.lang.UnsupportedClassVersionError:
+//            com/mycompany/javau/feb_29 has been compiled by a more recent version of the Java Runtime (class file version 63.0),
+//            this version of the Java Runtime only recognizes class file versions up to 60.0 Command execution failed.
+    
     public static void main(String[] args) {
         
-                String hola [] ={"hola","hello"};
+        Character homero = new Character("homero",39,'M', "A la grande le puse cuca");
+        Character bart = new Character("Barto",10,'M', "AY CARAMBA");
         
-        for(String saludo:hola){
-            System.out.println("saludo = " + saludo);
+        homero.saySomething();
+        bart.saySomething();
+       
+    }
+    
+      static class  Character{
+        String name;
+        int age;
+        char gender;
+        String phrase;
+
+        public  Character(String name, int age, char gender, String phrase) {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.phrase = phrase;
+        }      
+        
+        public  void saySomething() {
+            System.out.println(this.phrase);
         }
     }
 }
